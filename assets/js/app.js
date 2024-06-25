4651ab833c3b3c4bad977a916e9cd6de96aea6cd
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2024-06-25 17:32:32 +0530

    work on about us page mark up and css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
 const cl = console.log;
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    navText: ['<i class="fa-solid fa-angles-left fa-2x"></i>', '<i class="fa-solid fa-angles-right fa-2x"></i>'],
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            dots:false,
+        },
+        600:{
+            items:2,
+            dots:false,
+            nav:false,
+        },
+        1000:{
+            items:4,
+            nav:true,
+            dots:true,
+        }
+    }
+})
